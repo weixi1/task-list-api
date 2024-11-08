0839c41a6e4c783660912e47994ab830eddbac4c
Weixi He <heweixiguge@gmai.com>
2024-11-08 11:31:38 -0500

    add wave 6 test

--- tests/test_wave_06.py
+++ tests/test_wave_06.py
@@ -50,8 +50,9 @@ def test_get_tasks_for_specific_goal_no_goal(client):
 
     # Assert
     assert response.status_code == 404
+    assert response_body == {"message": f"Goal not found"}
 
-    raise Exception("Complete test with assertion about response body")
+    #raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
